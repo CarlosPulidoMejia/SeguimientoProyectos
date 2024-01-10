ec8ca6521cdbdcac56ea2dfd9db62b7420e9258c
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-01-10 14:17:55 -0600

    links ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CdaComponent } from './cda/cda.component';
-import { CertificacionesComponent } from './certificaciones/certificaciones.component';
 import { DevolucionesComponent } from './devoluciones/devoluciones.component';
 import { EnvioOrdenesComponent } from './envio-ordenes/envio-ordenes.component';
+import { CertificacionesComponent } from './certificaciones/certificaciones.component';
 
 ///BIM/Cdas
 
 const routes: Routes = [
+  { path: '', component: DevolucionesComponent, pathMatch: 'full'},
+  { path: 'Certificaciones/Envio', component:  EnvioOrdenesComponent, pathMatch: 'full'},
   { path: 'Certificaciones/BIM', component: CertificacionesComponent },
-  { path: '', component: EnvioOrdenesComponent, pathMatch: 'full'},
-  { path: 'Certificaciones/Devoluciones', component: DevolucionesComponent, pathMatch: 'full'},
-  { path: 'BIM/Cdas', component: CdaComponent, pathMatch: 'full'},
   { path: '**',redirectTo:''}
 ];
 
