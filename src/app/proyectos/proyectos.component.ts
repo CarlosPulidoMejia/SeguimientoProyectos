c12beee0892f7d9213f353dca4114ca52b40d866
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-01-19 14:53:00 -0600

    catalogos en tabla

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -83,10 +83,10 @@ export class ProyectosBauComponent implements OnInit {
   fechaFinProyecto: any;
   tipoDocumentacion: number;
   claseBoton: string;
-  faseProyecto: any;
-  estadoProy: any;
-  dependenciaProy: any;
-  docProy: any;
+  faseProy: number;
+  estadoProy: number;
+  dependenciaProy: number;
+  documentacionProy: any;
   hrsAtencion: any;
 
 
@@ -196,9 +196,18 @@ export class ProyectosBauComponent implements OnInit {
     }
   }
   
-  guardarCambio(){
+  guardarCambio(proyecto){
+    console.log(proyecto);
+    
+    let filtroDoc = this.listatipoDocumentacion.filter( value => value.documentacion == proyecto.docProy ); 
+    let filtroDep = this.listatipoDependencia.filter( value => value.tipoDependencia == proyecto.dependenciaProy);
+    let filtroEst = this.listatipoEstado.filter( value => value.tipoEstado == proyecto.estadoProy );
+    let filtroFas = this.listatipoFase.filter( value => value.fase == proyecto.faseProy );
     this.requestGuardar = {
-      documentacion: this.docProy,
+      documentacion: filtroDoc[0].idDocumentacion,
+      dependencia: filtroDep[0].idDependencia,
+      estado: filtroEst[0].idTipoEstado,
+      fase: filtroFas[0].idFase
     }
     console.log('Guardar Cambio', this.requestGuardar);
     
@@ -224,8 +233,6 @@ export class ProyectosBauComponent implements OnInit {
     this.proyectoBauService.getTipoDocumentacion().subscribe(
       data => {
         this.listatipoDocumentacion = data;
-        console.log(this.listatipoDocumentacion);
-        
       },
       err => {
         console.log(err)
@@ -248,8 +255,6 @@ export class ProyectosBauComponent implements OnInit {
     this.proyectoBauService.getTipoEstado().subscribe(
       data => {
         this.listatipoEstado = data;
-        console.log(this.listatipoEstado);
-        
       },
       err => {
         console.log(err)
@@ -298,309 +303,24 @@ export class ProyectosBauComponent implements OnInit {
     });
   }
 
-  /*listaBD() {
-    console.log("Bases Id")
-    this.datosService.listaBase().subscribe({
-      next: (data) => {
-        this.listaBase = data;
-        console.log(this.listaBase)
-
-      },
-      error: (e) => {
-        console.log(e)
-      }
-    })
-  }*/
-
-  /*getPagosDevolucion() {
-    this.pagosService.getAllPagos().subscribe({
-      next: (data) => {
-        this.listaPago = data;
-        console.log(this.listaPago)
-        this.listaPagoDev = this.listaPago.filter(x => x.tipo === 'D')
-        this.listaPago = this.listaPago.filter(x => x.aplicadevolucion === 'S')
-        console.log(this.listaPago)
-
-      },
-      error: (e) => {
-        console.log(e)
-      }
-    })
-  }*/
-
-  selectBase() {
-    let baseId = this.listaBase.find(x => x.id_Datasource === this.catBases)
-
-    this.id_Data = baseId.id_Datasource;
-    this.ip_Data = baseId.ip;
-    this.port_Data = baseId.puerto;
-    this.name_Data = baseId.nombre_Base;
-    /*this.datoBases.base = baseId.base;
-    this.datoBases.puerto = baseId.puerto;*/
+  modelChangeDocumentacion(e) {
+    //this.documentacionProy = e;
+    let filtro = this.listatipoDocumentacion.filter( value => value.documentacion == e );
+    this.documentacionProy = filtro[0].idDocumentacion
   }
 
-  catTipoRetorno() {
-    console.log(this.selectedtipo)
-    if (this.selectedtipo == "17" || this.selectedtipo == "23") {
-      //this.tipoPagoDevolucion = "Normal"
-      //this.selectedDateInicio = this.selectedDateInicio
-      this.habilitarFecha = true;
-    } else {
-      //this.tipoPagoDevolucion = "Exptemporaneo"
-      //this.selectedDateFin = this.selectedDateFin
-      this.habilitarFecha = false;
-    }
+  modelChangeDependencia(e){
+    let filtro = this.listatipoDependencia.filter( value => value.tipoDependencia == e)
+    this.dependenciaProy = filtro[0].idDependencia
   }
 
-  getOrdenesDevolucion() {
-
-    if (this.selectedtipo == "17" || this.selectedtipo == "23") {
-      this.tipoPagoDevolucion = "Normal"
-      //this.habilitarFecha = false;
-    } else {
-      this.tipoPagoDevolucion = "Extemporaneo"
-      //this.habilitarFecha = true;
-    }
-
-    this.requestBusqueda = {
-      datasource: {
-        id: this.id_Data
-      },
-      tipo: this.tipoPagoDevolucion,
-      numeroRegistros: this.numRegistros,
-      tipoPago: this.selectedTipoPago.clave,
-      concepto: this.concepto,
-      instancia: this.instancia,
-      fechaInicio: this.selectedDateInicio,
-      fechaFin: this.selectedDateFin,
-      orpNumeroDev: this.orpNumeroDev,
-      montoDev: this.montoDev
-    }
-
-    console.log(this.requestBusqueda)
-
-    Swal.fire({
-      title: "Buscando registros...",
-      text: "Espere un momento",
-      imageUrl: "../../assets/progress.gif",
-      imageWidth: 160,
-      imageHeight: 160,
-      showConfirmButton: false,
-      allowOutsideClick: false
-    });
-
-    this.proyectoBauService.getProyectos(this.requestBusqueda).subscribe(data => {
-      console.log(data)
-
-      this.listaProyectos = data;
-
-      $('#tablaDevoluciones').dataTable().fnDestroy();
-
-
-      $('#tablaDevoluciones tbody').on('click', 'tr', function () {
-        $(this).toggleClass('selected');
-      });
-
-
-    },
-      err => {
-        console.log(err)
-      },
-      () => {
-
-        setTimeout(() => {
-          $('#tablaDevoluciones').DataTable({
-            language: {
-              url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/es-mx.json'
-            },
-            pageLength: 10,
-            responsive: true
-          });
-        }, 1);
-
-        console.log("Complete function triggered.")
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Busqueda completada',
-          showConfirmButton: false
-        })
-      }
-    );
-
+  modelChangeEstado(e){
+    let filtro = this.listatipoEstado.filter( value => value.tipoEstado == e)
+    this.estadoProy = filtro[0].idTipoEstado
   }
 
-  /*selectAll() {
-    for (var i = 0; i < this.listaProyectos.length; i++) {
-      this.listaProyectos[i].seleccion = this.selectedAll;
-    }
-  }*/
-
-  /*chkDevoluciones() {
-    this.selectedAct = this.listaProyectos.filter(value => {
-      return value.seleccion
-    });
-
-    this.enviarListaDevolucion = this.selectedAct;
-
-    this.enviarListaDevolucion.map(x => {
-      x.pagoDev = this.selectedtipo
-    });
-
-    this.sendListaDevolucion = {
-      datasource: {
-        id: this.id_Data
-      },
-      devoluciones: this.enviarListaDevolucion
-    }
-
-    Swal.fire({
-      title: "Aplicando retornos...",
-      text: "Espere un momento",
-      imageUrl: "../../assets/progress.gif",
-      imageWidth: 160,
-      imageHeight: 160,
-      showConfirmButton: false,
-      allowOutsideClick: false
-    });
-
-    this.proyectoBauService.postListaProyectos(this.sendListaDevolucion).subscribe({
-      next: (data) => {
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Retornos aplicados correctamente',
-          timer: 1000,
-          showConfirmButton: false
-        })
-        this.limpiarTablas();
-      },
-      error: (e) => {
-        console.log(e)
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Ocurrio un error al realizar retornos, verifique',
-          timer: 1000,
-          showConfirmButton: false
-        })
-      }
-    })*/
-    //this.listaDevolucion = this.listaDevolucion.filter(x => x.numero === this.enviarListaDevolucion.filter(d => d.numero));
-
-
-    /*for(let i = 0; this.listaDevolucion.length;i++){
-      if(this.listaDevolucion[i].numero==this.enviarListaDevolucion.filter(z => z.numero)){
-        console.log(this.listaDevolucion.splice(i--,1))
-      }
-    }*/
-
-    //this.listaDevolucion = this.listaDevolucion.filter(x => x.row != this.enviarListaDevolucion.filter( x = x.row));
-
-    /*this.sendListaDevolucion.forEach(function (obj){
-      this.listaDevolucion = this.listaDevolucion.
-    })*/
-
-    //console.log(this.sendListaDevolucion)
-    //console.log("--")
-    //console.log(this.listaDevolucion)
-
-
-  //}
-
-  /*limpiarTablas() {
-    this.selectedAll = false;
-    $('#tablaDevoluciones').dataTable().fnDestroy();
-
-    setTimeout(() => {
-      $('#tablaDevoluciones').DataTable({
-        language: {
-          url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/es-mx.json'
-        },
-        pageLength: 10,
-        responsive: true
-      });
-    }, 1);
-
-    this.enviarListaDevolucion.forEach(dtx => {
-      this.listaProyectos = this.listaProyectos.filter(item => item.numero !== dtx.numero)
-    })
-  }*/
-
-  /**********
-   * 25/04/2023 
-   * Realizar varios retornos
-   */
-  /*getOrdenesDevolucionVarios() {
-
-    if (this.selectedtipo == "17" || this.selectedtipo == "23") {
-      this.tipoPagoDevolucion = "Normal"
-      //this.habilitarFecha = false;
-    } else {
-      this.tipoPagoDevolucion = "Extemporaneo"
-      //this.habilitarFecha = true;
-    }
-
-    this.requestBusqueda = {
-      datasource: {
-        id: this.id_Data
-      },
-      tipo: this.tipoPagoDevolucion,
-      concepto: this.concepto,
-      fechaInicio: this.selectedDateInicio,
-      fechaFin: this.selectedDateFin,
-      orpNumeroDev: this.orpNumeroDev,
-      montoDev: this.montoDev
-    }
-    
-
-    Swal.fire({
-      title: "Buscando registros...",
-      text: "Espere un momento",
-      imageUrl: "../../assets/progress.gif",
-      imageWidth: 160,
-      imageHeight: 160,
-      showConfirmButton: false,
-      allowOutsideClick: false
-    });
-
-    this.proyectoBauService.getDevolucionesVarios(this.requestBusqueda).subscribe({
-      next: (data) => {
-
-        this.listaProyectos = data;
-
-        $('#tablaDevoluciones').dataTable().fnDestroy();
-
-
-        $('#tablaDevoluciones tbody').on('click', 'tr', function () {
-          $(this).toggleClass('selected');
-        });
-
-        setTimeout(() => {
-          $('#tablaDevoluciones').DataTable({
-            language: {
-              url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/es-mx.json'
-            },
-            pageLength: 10,
-            responsive: true
-          });
-        }, 1);
-
-
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Busqueda completada varios',
-          showConfirmButton: false
-        })
-
-
-      },
-      error: (e) => {
-
-        console.log(e)
-      }
-    })
-  }*/
-
+  modelChangeFase(e){
+    let filtro = this.listatipoFase.filter( value => value.fase == e)
+    this.faseProy = filtro[0].idFase
+  }
 }
