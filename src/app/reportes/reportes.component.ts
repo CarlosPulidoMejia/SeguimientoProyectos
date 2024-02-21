192eb21c034b334ff0de3dcc70d32c94577c590b
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-02-21 17:44:38 -0600

    inicio de grafica

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -5,6 +5,10 @@ import { HttpEventType, HttpResponse } from '@angular/common/http';
 //import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { HttpClient } from '@angular/common/http';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+
 declare const $: any;
 
 @Component({
@@ -14,8 +18,12 @@ declare const $: any;
 })
 export class ReportesComponent implements OnInit {
 /**
- * 
+ * Grafica
  */
+view: [number,number] = [600,500];
+// options
+xAxis: boolean = true;
+yAxis: boolean = true;
   message: string[] = [];
 
   fileInfos?: Observable<any>;
@@ -24,17 +32,79 @@ export class ReportesComponent implements OnInit {
   proyectos: boolean;
   tickets: boolean;
   resumen: boolean;
+  grafica: boolean;
+  proyecto: any;
 
   constructor(private httpClient: HttpClient) { }
 
+  multi = []
+
+  multidos = []
   /*Valores matriz de cuentas*/
 
 
   /* fin valores matriz de cuentas*/
   ngOnInit() {
-    this.proyectos = true;
+    this.proyectos = false;
     this.tickets = false;
     this.resumen = false;
+    this.grafica = true;
+    this.proyecto = [
+      {
+        nombre:'Hrs Semana',
+        value: 10
+      },
+      {
+        nombre: 'En Tiempo',
+        value: 29,
+      },
+      {
+        name: 'Documentación',
+        value: 28,
+      },
+    ]
+    this.multi = [
+      {
+        name: 'Proyectos',
+        series: [
+          {
+            name: 'Hrs Semana',
+            value: this.proyecto[0].value,
+          },
+          {
+            name: 'En Tiempo',
+            value: this.proyecto[1].value,
+          },
+          {
+            name: 'Documentación',
+            value: this.proyecto[2].value,
+          },
+        ],
+      },
+    ]
+    this.multidos = [
+      {
+        name: 'Tickets',
+        series: [
+          {
+            name: 'Hrs Semana',
+            value: 10,
+          },
+          {
+            name: 'Bloqueante',
+            value: 29,
+          },
+          {
+            name: 'Controlado',
+            value: 28,
+          },
+          {
+            name: 'Recurrente',
+            value: 28,
+          },
+        ],
+      },
+    ]
   }
 
   showProyectos(){
@@ -51,6 +121,13 @@ export class ReportesComponent implements OnInit {
     this.proyectos = false;
     this.tickets = false;
     this.resumen = true;
+  }
+
+  showGrafica(){
+    this.proyectos = false;
+    this.tickets = false;
+    this.resumen = false;
+    this.grafica = true;
   }
 }
 
