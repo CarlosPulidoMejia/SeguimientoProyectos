7fcca4cad9b8ba55dba848018490c793e77bfb62
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-03-15 15:32:54 -0600

    .

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -56,9 +56,9 @@ export class ReportesComponent implements OnInit {
     this.classBotonTickets = 'btn btn-success mx-2'
     this.classBotonResumen = 'btn btn-success'
     this.classBotonGrafica = 'btn btn-success mx-2'
-    this.proyectos = false;
+    this.proyectos = true;
     this.tickets = false;
-    this.resumen = true;
+    this.resumen = false;
     this.grafica = false;
     this.proyecto = [
       {
