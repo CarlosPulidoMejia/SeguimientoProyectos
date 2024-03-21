24589c7e795c2ba29296aea5073f20b56fd88b87
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-03-21 13:31:09 -0600

    solo una ip

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,5 +1,5 @@
 'use strict';
 
-export const URL_APIA : string ='http://172.32.228.61:8080/';
+//export const URL_APIA : string ='http://172.32.228.61:8080/';
 export const URL_API : string ='http://172.32.228.61:8080/';
 
